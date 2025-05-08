015af0703e2a0e90d827ba4e989617d2cf98f732
MassageByVanessa <19t1durh@sandon.essex.sch.uk>
2025-05-08 11:53:44 +0100

    Update PasswordValidation.js
    updating validation so user cannot input a blank username

--- PasswordValidation.js
+++ PasswordValidation.js
@@ -1,5 +1,9 @@
 function validatePassword() {
   password = document.getElementById("PasswordInput").value // creates a variable that gets the answer entered by the user
+  if (password == "") { // is the password equal to nothing?
+    alert("error!")
+    return false;
+  }
 }
 
 function alertTesting() {
