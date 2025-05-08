51c3e09e3f46d17c16420b3b4a5fb8c26929f8f1
MassageByVanessa <19t1durh@sandon.essex.sch.uk>
2025-05-08 12:11:22 +0100

    Update PasswordValidation.js
    creating validation so the password has to be more than 8 characters

--- PasswordValidation.js
+++ PasswordValidation.js
@@ -1,11 +1,11 @@
 function validatePassword() {
   password = document.getElementById("PasswordInput").value // creates a variable that gets the answer entered by the user
+  passwordLength = password.length; // creates a variable for the password length - this will be used for the validation.
   if (password == "") { // is the password equal to nothing?
-    alert("error!")
+    alert("error!");
+    return false;
+  else if (passwordLength < 8) { // is the password less than 8 characters?
+    alert("password is too short!");
     return false;
   }
-}
-
-function alertTesting() {
-  alert("error!"); 
 }
