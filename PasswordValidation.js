37084e7c30f1fb2e0bdadb451727f5e885f6b2b7
MassageByVanessa <19t1durh@sandon.essex.sch.uk>
2025-05-08 11:30:58 +0100

    Update PasswordValidation.js
    created a new function for testing purposes

--- PasswordValidation.js
+++ PasswordValidation.js
@@ -1,2 +1,7 @@
 function validatePassword() {
   password = document.getElementById("PasswordInput").value // creates a variable that gets the answer entered by the user
+}
+
+function alertTesting() {
+  alert("error!"); 
+}
