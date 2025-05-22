a7f66a9d65d246ef725e446a78e273bb19f4285f
MassageByVanessa <19t1durh@sandon.essex.sch.uk>
2025-05-22 11:33:15 +0100

    Update PasswordValidation.js
    changing the error for a blank password to something more specific

--- PasswordValidation.js
+++ PasswordValidation.js
@@ -2,7 +2,7 @@ function validatePassword() {
   password = document.getElementById("PasswordInput").value // creates a variable that gets the answer entered by the user
   passwordLength = password.length; // creates a variable for the password length - this will be used for the validation.
   if (password == "") { // is the password equal to nothing?
-    alert("error!");
+    alert("error! Password is equal to nothing!");
     return false;
   }
   else if (passwordLength < 8) { // is the password less than 8 characters?
