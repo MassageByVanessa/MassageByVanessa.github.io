9fac96724d8742e2512a222e69af61c22efa072a
MassageByVanessa <19t1durh@sandon.essex.sch.uk>
2025-05-22 11:50:25 +0100

    Update PasswordValidation.js
    creating an uppercase and lowercase variable

--- PasswordValidation.js
+++ PasswordValidation.js
@@ -1,6 +1,10 @@
 function validatePassword() {
   password = document.getElementById("PasswordInput").value // creates a variable that gets the answer entered by the user
   passwordLength = password.length; // creates a variable for the password length - this will be used for the validation.
+  let UpperCase = /[A-Z]/.test(password);
+  let LowerCase = /[a-z]/.test(password);
+  let Space = /\s/.test(password);
+  
   if (password == "") { // is the password equal to nothing?
     alert("error! Password is equal to nothing!");
     return false;
@@ -8,5 +12,13 @@ function validatePassword() {
   else if (passwordLength < 8) { // is the password less than 8 characters?
     alert("password is too short!");
     return false;
+  }
+  else if (!UpperCase) {
+    alert("Password needs to contain an uppercase letter!")
+    return false;
+  }
+  else if (!LowerCase) {
+    alert("Password needs to contain a lowercase letter!")
+    return false:
   }
 }
